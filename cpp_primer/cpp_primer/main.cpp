1def9f4c79aa149af811e6ba7b7f2128066404f9
wangxiaorui19 <wangxiaorui19@jd.com>
2022-05-22 15:04:37 +0800

    建立工程

---
+++ cpp_primer/cpp_primer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_primer
+//
+//  Created by wangxiaorui19 on 2022/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
